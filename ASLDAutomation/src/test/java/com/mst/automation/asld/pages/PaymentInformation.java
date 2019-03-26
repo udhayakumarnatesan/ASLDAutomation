5625a257092c0c417a9953a63f9e7df4375e41fe
udhayakumarnatesan <udhayakumar@mstsolutions.com>
2019-03-26 11:03:10 +0530

    Today's commit(03/26/2019)

--- ASLDAutomation/src/test/java/com/mst/automation/asld/pages/PaymentInformation.java
+++ ASLDAutomation/src/test/java/com/mst/automation/asld/pages/PaymentInformation.java
@@ -1,3 +1,11 @@
+/*
+ * @author Udhayakumar N
+ * Created date: Mar 08,2019
+ * Last Edited by: Udhayakumar N
+ * Last Edited date: 
+ * Description: 
+ */
+
 package com.mst.automation.asld.pages;
 
 import java.util.concurrent.TimeUnit;
@@ -13,8 +21,8 @@ import com.mst.automation.asld.excelutility.DataUtils;
 import com.mst.automation.asld.extentreport.Report;
 
 public class PaymentInformation {
-	
-	@FindBy(xpath = ".//a[1][text()='Switch to Lightning Experience']") 
+
+	@FindBy(xpath = ".//a[1][text()='Switch to Lightning Experience']")
 	public WebElement switchtolightning;
 
 	@FindBy(how = How.XPATH, using = ".//nav/button[@type='button']")
@@ -22,47 +30,49 @@ public class PaymentInformation {
 
 	@FindBy(how = How.CSS, using = "input[placeholder='Search apps or items...']")
 	public WebElement appLauncherSearch;
-	
+
 	@FindBy(how = How.XPATH, using = ".//span/mark[text()='Payment Information']")
 	public WebElement accessPaymentInformation;
-	
+
 	@FindBy(how = How.XPATH, using = ".//a/div[text()='New']")
 	public WebElement newbutton;
-	
+
 	@FindBy(how = How.XPATH, using = ".//span[text()='Payment Type']//following::div[1]/div/div/div/a")
 	public WebElement Paymenttypefield;
-	
+
 	@FindBy(how = How.XPATH, using = ".//div[@class='select-options popupTargetContainer uiPopupTarget uiMenuList uiMenuList--default uiMenuList--left uiMenuList--short visible positioned']")
 	public WebElement Paymenttypefieldvalue;
-	 	
+
 	public static void paymentInformationObject(WebDriver driver, String sheetName, String tcID) throws Exception {
 		PaymentInformation page = PageFactory.initElements(driver, PaymentInformation.class);
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		boolean isPresent = driver.findElements(By.xpath(".//*[@id='createNewButton']")).size()> 0;
-        if(isPresent==true) {
+		boolean isPresent = driver.findElements(By.xpath(".//*[@id='createNewButton']")).size() > 0;
+		if (isPresent == true) {
 			page.switchtolightning.click();
 			driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
 			page.appLauncherIcon.click();
-		} 
-        else if(isPresent ==false) {
-        	page.appLauncherIcon.click();
+		} else if (isPresent == false) {
+			page.appLauncherIcon.click();
 		}
-        page.appLauncherSearch.sendKeys(DataUtils.readExcel(sheetName, tcID, "Object Name"));
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-        page.accessPaymentInformation.click();
-        Thread.sleep(3000);;
-        page.newbutton.click();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        page.Paymenttypefield.click();
-        verifyValue(page.Paymenttypefieldvalue, DataUtils.readExcel(sheetName, tcID, "Picklist value"));
-}
+		page.appLauncherSearch.sendKeys(DataUtils.readExcel(sheetName, tcID, "Object Name"));
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+		page.accessPaymentInformation.click();
+		Thread.sleep(3000);
+		page.newbutton.click();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		page.Paymenttypefield.click();
+		verifyValue(page.Paymenttypefieldvalue, DataUtils.readExcel(sheetName, tcID, "Picklist value"));
+	}
+	//This method is used to generate the Report
 	public static void verifyValue(WebElement ElementID, String value) throws Exception {
 		String ExpectedData = value;
 		String portaldata = ElementID.getText();
 		if (portaldata.equals(ExpectedData)) {
-			Report.testStepStatus("Value mached", "Pass", "Payment field value is " + ExpectedData + " and Record value is " + portaldata + " both are same verified");
+			Report.testStepStatus("Value mached", "Pass", "Payment field value is " + ExpectedData
+					+ " and Record value is " + portaldata + " both are same verified");
 		} else {
-			Report.testStepStatus("Value not mached", "Fail", "Payment field value is " + ExpectedData + " and Record value is " + portaldata + "both are different not verified");
+			Report.testStepStatus("Value not mached", "Fail", "Payment field value is " + ExpectedData
+					+ " and Record value is " + portaldata + "both are different not verified");
 		}
 	}
 }
